38697e6728e8ae5713d9dd42779fd6d2ab80c8fc
Ahmet Öztürk Şahin <83590669+ahmetsahn@users.noreply.github.com>
2023-05-12 17:46:01 +0300

    36

---
+++ DergiProjesi/DergiProjesi/Controllers/JournalController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DergiProjesi.Models;
+
+namespace DergiProjesi.Controllers
+{
+    public class JournalController : Controller
+    {
+        private JournalContext db = new JournalContext();
+
+        // GET: Journal
+        public ActionResult Index()
+        {
+            var journals = db.Journals.Include(j => j.Category);
+            return View(journals.ToList());
+        }
+
+        // GET: Journal/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Journal journal = db.Journals.Find(id);
+            if (journal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(journal);
+        }
+
+        // GET: Journal/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName");
+            return View();
+        }
+
+        // POST: Journal/Create
+        // Aşırı gönderim saldırılarından korunmak için, bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,ImageURL,CategoryId")] Journal journal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Journals.Add(journal);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", journal.CategoryId);
+            return View(journal);
+        }
+
+        // GET: Journal/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Journal journal = db.Journals.Find(id);
+            if (journal == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", journal.CategoryId);
+            return View(journal);
+        }
+
+        // POST: Journal/Edit/5
+        // Aşırı gönderim saldırılarından korunmak için, bağlamak istediğiniz belirli özellikleri etkinleştirin, 
+        // daha fazla bilgi için bkz. https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,ImageURL,CategoryId")] Journal journal)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(journal).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryId = new SelectList(db.Categories, "Id", "CategoryName", journal.CategoryId);
+            return View(journal);
+        }
+
+        // GET: Journal/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Journal journal = db.Journals.Find(id);
+            if (journal == null)
+            {
+                return HttpNotFound();
+            }
+            return View(journal);
+        }
+
+        // POST: Journal/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Journal journal = db.Journals.Find(id);
+            db.Journals.Remove(journal);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
